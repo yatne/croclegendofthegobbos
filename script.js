75630fc5caad3ee5155c0c89dcb4cb21983fdec3
yatne <antonimaciej@gmail.com>
2022-06-17 12:23:22 +0200

    init

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var answer = 'lllldrrlldrdlur';
+var counter = 0;
+
+function up() {
+    checkAnswer('u');
+}
+function down() {
+    checkAnswer('d');
+}
+function left() {
+    checkAnswer('l');
+}
+function right() {
+    checkAnswer('r');
+}
+
+function checkAnswer(char) {
+    if (answer[counter] === char) {
+        goodAnswer();
+    } else {
+        wrongAnswer();
+    }
+}
+
+function goodAnswer() {
+    if (counter < answer.length - 1) {
+        counter = counter + 1;
+    } else {
+        alert('bondziorno!');
+    }
+}
+
+function wrongAnswer() {
+    document.getElementsByClassName('content')[0].style = 'background-color: red;';
+    setTimeout(() => {
+        document.getElementsByClassName('content')[0].style = 'background-color: greenyellow;';
+    }, 200)
+    counter = 0;
+}
